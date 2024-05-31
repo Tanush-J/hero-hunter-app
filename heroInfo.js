c81060ab42ca2e7cbcd8928a7b357f0b2f0582c6
Tanush-J <Tanush-J@users.noreply.github.com>
2024-05-31 15:30:40 +0000

    deploy: e734183b71832ce2c692a23de68d97064fc1acf9

--- heroInfo.js
+++ heroInfo.js
@@ -1,6 +1,3 @@
-import { publicKey, privateKey } from './secrets.js'
-const md5 = CryptoJS.MD5;
-
 const heroName = document.getElementById('heroName');
 const heroDescription = document.getElementById('heroDescription')
 const heroImg = document.getElementById('heroImg');
@@ -28,32 +25,6 @@ const heroDescriptionLoader = (description) => {
     }
 }
 
-// const fetchEventData = (URI, id) => {
-//     const date = new Date().getTime();
-//     const hash = md5(date + privateKey + publicKey).toString();
-//     fetch(`${URI}?apikey=${publicKey}&hash=${hash}`)
-//     .then((res) => {
-//         if(!res.ok){
-//             throw new Error('Could not fetch series data');
-//         }
-//         return res.json();
-//     })
-//     .then(res => {
-//         const result = res.data.results[0];
-//         const bodyEle = document.getElementById(id);
-//         const eventImg = document.createElement('img');
-//         eventImg.src = `${result.thumbnail.path}.${result.thumbnail.extension}`;
-//         const para = document.createElement('p');
-//         para.textContent = result.description;
-//         bodyEle.appendChild(eventImg);
-//         bodyEle.appendChild(para);
-//     })
-//     .catch(err => {
-//         console.error('Series fetch Error: '+err);
-//     })
-    
-// }
-
 const accordianEventLoader = (eventArr, id) => {
     const parentCont = document.getElementById(id)
     if(eventArr.length>0){
@@ -69,33 +40,6 @@ const accordianEventLoader = (eventArr, id) => {
         itemEle.textContent = 'No data avaliable!'
         parentCont.appendChild(itemEle)
     }
-    // eventArr.forEach((item, index) => {
-    //     const itemEle = document.createElement('div')
-    //     itemEle.classList.add('accordion-item');
-    //     const header = document.createElement('div');
-    //     header.classList.add('accordion-header')
-    //     const toggleBtn = document.createElement('button');
-    //     toggleBtn.classList.add('accordion-button', 'collapsed')
-    //     toggleBtn.type = 'button'
-    //     toggleBtn.setAttribute('data-bs-toggle',"collapse")
-    //     toggleBtn.setAttribute('data-bs-target',`#collapse${index}`);
-    //     toggleBtn.setAttribute('aria-expanded',"true");
-    //     toggleBtn.setAttribute('aria-controls',`collapse${index}`)
-    //     toggleBtn.textContent = item.name;
-    //     toggleBtn.addEventListener('click', () => fetchEventData(item.resourceURI, `${item.name}-${index}`))
-    //     const bodyWrapper = document.createElement('div')
-    //     bodyWrapper.id=`collapse${index}`
-    //     bodyWrapper.classList.add("accordion-collapse", "collapse");
-    //     bodyWrapper.setAttribute("data-bs-parent","#seriesContainer");
-    //     const body = document.createElement('div');
-    //     body.id = `${item.name}-${index}`;
-    //     body.classList.add("accordion-body");
-    //     bodyWrapper.appendChild(body);
-    //     itemEle.appendChild(header);
-    //     itemEle.appendChild(toggleBtn);
-    //     itemEle.appendChild(bodyWrapper);
-    //     parentCont.appendChild(itemEle);
-    // });
 }
 
 //event listener
